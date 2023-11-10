2630bef8ad46775492f8337a0e33035eab17a3db
tmaygon609 <tmaygon609@iesalmudeyne.es>
2023-11-10 10:58:53 +0100

    Version del fichero HolaMundo.java con colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,10 @@
 /**
  * Muestra por pantalla la frase ¡Hola mundo! * 
  */
-
 public class HolaMundo {
     public static void main(String[] args){
+        String rojo = "\033[32m]";
+        String azul = "\033[34m]"
         System.out.prinln("¡Hola mundo!");
         System.out.prinln("Aquí estamos haciendo pruebas en Git.");
     }
