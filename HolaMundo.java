5626db9e828c0bed13d99b59e7abcbdb89109e42
tmaygon609 <tmaygon609@iesalmudeyne.es>
2023-11-14 14:10:12 +0100

    Cambio de un ;

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,7 +4,7 @@
 public class HolaMundo {
     public static void main(String[] args){
         String rojo = "\033[32m]";
-        String azul = "\033[34m]"
+        String azul = "\033[34m]";
         System.out.prinln("¡Hola mundo!");
         System.out.prinln("Aquí estamos haciendo pruebas en Git.");
     }
