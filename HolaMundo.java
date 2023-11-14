3c1d1eb367f7369beee5cc223095de5eb00212cd
tmaygon609 <tmaygon609@iesalmudeyne.es>
2023-11-14 14:22:45 +0100

    Una mierda

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
     public static void main(String[] args){
         String rojo = "\033[32m";
         String azul = "\033[34m";
-        System.out.prinln("¡Hola mundo!");
-        System.out.prinln("Aquí estamos haciendo pruebas en Git.");
+        System.out.prinln("Hola mundo!");
+        System.out.prinln("Aqui estamos haciendo pruebas en Git.");
     }
 }
