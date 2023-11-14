9645871a1f0d32dbab6dc2d999845df6b3cac4ba
tmaygon609 <tmaygon609@iesalmudeyne.es>
2023-11-14 14:19:50 +0100

    sadfasdf

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,8 +3,8 @@
  */
 public class HolaMundo {
     public static void main(String[] args){
-        String rojo = "\033[32m]";
-        String azul = "\033[34m]";
+        String rojo = "\033[32m";
+        String azul = "\033[34m";
         System.out.prinln("¡Hola mundo!");
         System.out.prinln("Aquí estamos haciendo pruebas en Git.");
     }
